bec51412c7b8395bf13822ff37915bc221014638
rowenhama <tafadzwarhama@gmail.com>
2023-02-21 18:08:27 +0200

    input your own api key

--- lib/static/static_values.dart
+++ lib/static/static_values.dart
@@ -3,7 +3,7 @@ class StaticValues {
 
   static const String apiUrl = 'https://newsapi.org/v2/top-headlines';
   //TODO Fill your api key
-  static const String apiKey = '1f8addb9544b4e48baa4b6a7aaef6e47';
+  static const String apiKey = '';
   static const String apiCountry = 'za';
 
 }
